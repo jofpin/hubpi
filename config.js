b490b3c9b2e12b59ff0a4ad6d99ee2a89ff49a5e
José Pino (Fraph) <mrjopino@gmail.com>
2014-04-27 11:56:38 -0500

    fix

---
+++ config.js
@@ -0,0 +1,7 @@
+$username = "Kanye West"; 
+$twitter = "kanyewest"; 
+$description = "I am a rapper and music producer";
+$avatar = "assets/img/avatar.png";
+$background_image = "assets/img/background.jpg";
+$background_color = "#45739C";
+$g_analytics = "UA-XXXXX-X";
