8c7541e01bc40f721bef6289a36319cea3daf9cd
José Pino <jofpin@gmail.com>
2014-07-24 14:39:02 -0500

    hubpi.post.js v1.0.1

--- assets/js/hubpi.post.js
+++ assets/js/hubpi.post.js
@@ -1,2 +1,84 @@
-/*! hubpi v1.0.1 |name: hubpi.post.js| (c) Copyright 2014 - Jose Pino, @jofpin | MIT | Date: 27/04/2014 | https://github.com/jofpin/hubpi */
-$(function(){"use-strict";var e=$("[data-hp-post]");if(typeof $.hubpi==="undefined"){$.hubpi={}}$.hubpi.get={noDATA:function(){if(e){this.ajaxHP()}else{e.html("There is no data :(")}},ajaxHP:function(){var t=this;$.ajaxSetup({cache:false});e.html('<span class="'+"hp_loader"+'">'+'<span class="'+"hp_loading"+'">'+"</span>"+"</span>");$.getJSON(e.data("hp-post"),function(n){var r=t.content_hubpers();$.each(n,function(e,n){r+=t.templatePOST(n)});e.html(r)}).error(function(t,n,r){e.html('<span class="'+"error-post-json"+'">'+"Error "+r+"</span>");console.log("Error : "+r)})},content_hubpers:function(){html="";return html},templatePOST:function(e){var t=e.content;var n=e.title;var r=e.date;html='<header class="'+"header-post"+'">'+'<h1 class="'+"title-post"+'" title="'+n+'">'+n+"</h1>"+"</header>"+'<section class="'+"post-content box"+'">'+'<div class="'+"date-post"+'"> '+r+"</div>"+"<article>"+t+"</article>"+"</section>";console.log("Title post : "+n+" | Date :"+r);return html}};$(document).ready(function(){$.hubpi.get.noDATA()})})();
+/*! hubpi v1.0.1 | (c) Copyright 2014 - Jose Pino, @jofpin | MIT | Date: 27/04/2014 | https://github.com/jofpin/hubpi */
+$(function() {
+  
+  "use-strict";
+
+  /* data hubpers with url of json posts */
+
+  var attrData  = $('[data-hp-post]');
+
+    if (typeof $.hubpi === "undefined") { 
+      $.hubpi = {};
+    } 
+  
+    // $.hubpi = {};
+
+      $.hubpi.get = {
+      noDATA: function() {
+        if (attrData) {
+          this.ajaxHP();
+        } 
+        else {
+          attrData.html("There is no data :(");
+        }
+      },
+
+      ajaxHP: function() { /* data get json and ajax not cache :) */
+        var self = this;
+        $.ajaxSetup({
+          cache: false
+        });
+
+      // loader of data content
+      attrData.html(
+                  '<span class="' + "hp_loader" +'">' +
+                  '<span class="' + "hp_loading" +'">' +
+                  '</span>' +
+                  '</span>'
+                  );
+      
+      $.getJSON(attrData.data('hp-post'),function(data) {
+        
+        var hp = self.content_hubpers(); 
+        
+        $.each(data,function(_,_item) {  
+          hp += self.templatePOST(_item);
+        });
+
+        attrData.html(hp);
+        
+      }).error(function(j,t,e) { // error load dataJSON :´(
+        attrData.html('<span class="' + "error-post-json" + '">' + "Error " + e + '</span>');
+        console.log('Error : ' + e);
+      });
+    },
+    
+    // your div here here :)
+    content_hubpers: function() {
+      html = ('');
+      return html;
+    },
+
+    templatePOST: function(data) { 
+      var content = data.content;
+      var title = data.title;
+      var date = data.date;
+
+      html = '<header class="' + "header-post" + '">' + 
+             '<h1 class="' + "title-post" + '" title="' + title + '">' + title + '</h1>' +
+             '</header>' +
+             '<section class="' + "post-content box" + '">' +
+             '<div class="' + "date-post" + '"> ' + date + '</div>' + 
+             '<article>' + content +
+             '</article>' +
+             '</section>';
+             console.log('Title post : ' + title + ' | Date :' + date);
+      return html;
+    }
+  };
+
+    $(document).ready(function() {
+      $.hubpi.get.noDATA();
+    });
+
+});
